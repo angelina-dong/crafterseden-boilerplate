efef03bb412dae56c0d033fd0b39abe0fb56d6a7
shrukumar <shrutika.skumar@gmail.com>
2023-04-15 11:10:59 -0400

    Update mary.py

--- flask-app/src/mary/mary.py
+++ flask-app/src/mary/mary.py
@@ -3,20 +3,79 @@ import json
 from src import db
 
 
-mary = Blueprint('customers', __name__)
+mary = Blueprint('mary', __name__)
 
 # Get all customers from the DB
-@mary.route('/mary', methods=['GET'])
-pass
+@mary.route('/products/<productID>', methods=['GET'])
+def get_product_details(productID):
+    cursor = db.get_db().cursor()
+    cursor.execute('select * from products where productid = {0}'.format(productID))
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+    
+
 
 # Get customer detail for customer with particular userID
 @mary.route('/mary/<userID>', methods=['GET'])
-<<<<<<< HEAD
 def get_smthg(userID):
     pass
-=======
 pass
 
-@mary.route('/mary/yarnProducts/weight/{weight}')
-pass
->>>>>>> e0aa61eb15808b89fbd0863fd412d2e8d31813cb
+@mary.route('/mary/yarnProducts/weight/<weight>', methods=[GET])
+def filter_by_weight(weight)
+    query = '''
+            SELECT *
+            FROM yarnProducts
+            WHERE yarnProducts.weight = weight
+            ORDER BY name ASC
+        '''
+    
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+
+    column_headers = [x[0] for x in cursor.description]
+    
+    json_data = []
+
+    # fetch all the data from the cursor
+    theData = cursor.fetchall()
+
+    # for each of the rows, zip the data elements together with
+    # the column headers. 
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+
+    return jsonify(json_data)
+
+@mary.route('/mary/yarnProducts/fiber/<fiber>', methods=[GET])
+def filter_by_weight(weight)
+    query = '''
+            SELECT *
+            FROM yarnProducts
+            WHERE yarnProducts.fiber = fiber
+            ORDER BY name ASC
+        '''
+    
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+
+    column_headers = [x[0] for x in cursor.description]
+    
+    json_data = []
+
+    # fetch all the data from the cursor
+    theData = cursor.fetchall()
+
+    # for each of the rows, zip the data elements together with
+    # the column headers. 
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+
+    return jsonify(json_data)
